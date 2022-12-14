d89180cfd4055f79ad06c7549c3b4da1cb6ef92e
KaterinkaVysotskaya <katerinka.vysotskaya@gmail.com>
2022-12-14 22:52:36 +0300

    antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, {Suspense, lazy, useEffect} from "react";
+import React, {Suspense, lazy, useEffect, useState} from "react";
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Link, Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import HeaderContainer from "./components/Header/HeaderContainer";
 import CustomizedSnackbars from "./outils/ErrorSnackBar";
 import {useDispatch} from "react-redux";
@@ -10,12 +10,56 @@ import {initializeAppTC} from "./redux/app-reducer";
 import {CircularProgress, LinearProgress} from "@mui/material";
 import {WithRouterWrap} from "./outils/withRouterWrap";
 import {UsersPage} from "./components/Users/UsersContainer";
+import 'antd/dist/reset.css';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import {Avatar, Breadcrumb, Col, Layout, Menu, Row, theme} from 'antd';
+import s from "./components/Navbar/Navbar.module.css";
+import Header from "./components/Header/Header";
+
+
 
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const Login = React.lazy(() => import('./components/login/Login'));
 const DialogsContainer = lazy(() => import("./components/dialogs/DialogsContainer"))
 
-function App() {
+const { Content, Footer, Sider } = Layout;
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+const profile = <NavLink  to='/profile' className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+const items: MenuItem[] = [
+    getItem('My profile', 'sub1', <UserOutlined />, [
+        getItem('profile','3',    ),
+        getItem('messages', '4'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('users', '6'), ]),
+    getItem('Files', '9', <FileOutlined />),
+];
+
+const App: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     const dispatch = useDispatch()
     const isInitialized = useAppSelector(state => state.app.isInitialized)
     const status = useAppSelector(state => state.app.status)
@@ -23,6 +67,7 @@ function App() {
     useEffect(() => {
         dispatch(initializeAppTC())
     }, [])
+
     if (!isInitialized) {
         return <div
             style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
@@ -31,33 +76,56 @@ function App() {
     }
     return (
         <BrowserRouter>
-            {status === 'loading' && <LinearProgress color="secondary"/>}
-            <div className='app-wrapper'>
-                <Navbar/>
-                <HeaderContainer/>
-                <CustomizedSnackbars/>
-                <div className='app-wrapper-content'>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Routes>
-                            <Route path='/' element={<ProfileContainer/>}/>
-                            <Route path='/users' element={<UsersPage/>}/>
-                            <Route path='/dialogs' element={<DialogsContainer/>}/>
-                            <Route path='/profile' element={<ProfileContainer/>}/>
-                            <Route path="/profile/:userId" element={<WithRouterWrap/>}/>
-                            <Route path="/login" element={<Login/>}/>
-                            <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
-                            <Route path="*" element={<Navigate to='/404'/>}/>
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+            </Sider>
+            <Layout className="site-layout">
+                <Header  />
 
-                            {/* <Route path ='/News' element={<News/>} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+
+                            {status === 'loading' && <LinearProgress color="secondary"/>}
+                            <div className='app-wrapper'>
+                                <Navbar/>
+                                {/*<HeaderContainer/>*/}
+                                <CustomizedSnackbars/>
+                                <div className='app-wrapper-content'>
+                                    <Suspense fallback={<div>Loading...</div>}>
+                                        <Routes>
+                                            <Route path='/' element={<ProfileContainer/>}/>
+                                            <Route path='/users' element={<UsersPage/>}/>
+                                            <Route path='/dialogs' element={<DialogsContainer/>}/>
+                                            <Route path='/profile' element={<ProfileContainer/>}/>
+                                            <Route path="/profile/:userId" element={<WithRouterWrap/>}/>
+                                            <Route path="/login" element={<Login/>}/>
+                                            <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
+                                            <Route path="*" element={<Navigate to='/404'/>}/>
+
+                                            {/* <Route path ='/News' element={<News/>} />
                              <Route path ='/Music' element={<Music/>} />
                              <Route path ='/Settings' element={<Settings/>} /> */}
-                        </Routes>
-                    </Suspense>
-                </div>
-            </div>
+                                        </Routes>
+                                    </Suspense>
+                                </div>
+                            </div>
+
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Samurai Social Network ©2022 Created by Catherine Vyssotskaya</Footer>
+            </Layout>
+        </Layout>
         </BrowserRouter>
     );
-}
+};
+
+
 
 export default App;
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,42 @@ import s from './Header.module.css' ;
 import { NavLink } from 'react-router-dom';
 import {HeaderPropsType} from "./HeaderContainer";
 import Button from "@mui/material/Button";
+import {Avatar, Col, Layout, Row} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+import 'antd/dist/reset.css';
+import {useAppSelector} from "../../redux/redux-store";
+import {useDispatch} from "react-redux";
+import {logout} from "../../redux/auth-reducer";
 
 
-const Header = React.memo((props: HeaderPropsType) => {
-    return <header className={s.header}>
-    <img alt='header' src='http://a.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fncaa%2F500%2F41.png' />
-  
-    <div className={s.loginBlock}>
-      { props.isAuth
-          ? <div> {props.login}  -  <Button onClick={props.logout}>Log out</Button> </div>
-          : <NavLink to={'/login'}><Button>Login</Button></NavLink>}
-      
-    </div>
-  </header>
+const Header = React.memo((props) => {
+   const isAuth = useAppSelector(state=>state.auth.isAuth)
+    const login = useAppSelector(state=>state.auth.login)
+    const dispatch = useDispatch()
+    const logoutCallback = ()=>{
+       dispatch(logout())
+    }
+    const { Header} = Layout;
+    return <Header >
+        <Row>
+            <Col span={20}>
+
+            </Col>
+            <Col>
+
+                <div className={s.loginBlock}>
+                    {isAuth
+                        ? <div>
+                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                            {login}  -  <Button onClick={logoutCallback}>Log out</Button> </div>
+                        : <NavLink to={'/login'}><Button>Login</Button></NavLink>}
+
+                </div>
+            </Col>
+        </Row>
+
+
+  </Header>
 })
 
 export default Header;
